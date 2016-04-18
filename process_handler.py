f0f80f40c8b793459765dfa45dc7e51a1cd72dca
Cristian Mtr <cristianmtr@users.noreply.github.com>
2016-04-18 13:19:09 +0200

    Update process_handler.py
    added popen_improved solution

--- process_handler.py
+++ process_handler.py
@@ -73,3 +73,52 @@ def shell_ex(cmd, print_output=True, outputfile=None, outerrfile=None):
         except:
             print "Failed to write stderr output to '%s'" % outerrfile
     return exitcode, stdout, stderr            
+
+
+def popen_improved(command, shell_cmd=False, print_output=False):
+    """
+    Parameters
+    ----------
+    command : array
+        array holding each element of the command line to run e.g. ["npm",\
+         "install"], ["npm","--version"]
+    shell_cmd : bool, optional
+        is the command a shell built-in and will it use env vars. Don't use\
+         on POSIX.
+        Windows only.
+        e.g. for "set", or "dir", or "where"
+    print_output : bool, optional
+        print output, err, exit_code to stdout of current process
+    Returns object with output, err, exit_code properties
+    """
+    class PopenReturn(object):
+        """
+        Attributes
+        ----------
+        err : string
+            stderr from process, stripped
+        exit_code : int
+            exit_code from process, stripped
+        output : string
+            stdout from process, stripped
+        """
+        def __init__(self, output, err, exit_code):
+            super(PopenReturn, self).__init__()
+            self.output = output
+            self.err = err
+            self.exit_code = exit_code
+    if print_output:
+        logger.info("EXECUTING '{}'".format(" ".join(command)))
+    process = Popen(command, stdout=PIPE, stderr=PIPE, shell=shell_cmd)
+    output = ""
+    err = ""
+    for line in process.stdout.readlines():
+        output += line
+    for error_line in process.stderr.readlines():
+        err += error_line
+    exit_code = process.wait()
+    if print_output:
+        logger.info("OUTPUT: {}".format(output))
+        logger.info("ERR: {}".format(err))
+        logger.info("EXIT CODE: {}".format(exit_code))
+    return PopenReturn(output.strip(), err.strip(), exit_code)
