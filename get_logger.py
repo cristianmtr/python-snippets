fd7fe7c71d05ce43057d1260f1ef1d61ccd8e927
Cristian Mtr <cristianmtr@users.noreply.github.com>
2016-05-02 14:52:12 +0200

    Update get_logger.py

--- get_logger.py
+++ get_logger.py
@@ -4,18 +4,23 @@ import sys
 
 
 def get_module_logger(name):
-    """Get a logger for the name module
+    """Get a logger for the module name
 
-    Parameters
-    ----------
-    name : str
-        the name of the module. Can be the builtin `__name__`
+    Args:
+        name (string): name of the module
 
-    Returns
-    -------
-    TYPE
-        (logger)[https://docs.python.org/2/library/logging.html#logging.Logger]\
-         instance.
+    Returns:
+        TYPE: (logger)[https://docs.python.org/2/library/logging.html#logging.Logger]\
+             instance.
+
     """
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M')
+    logger.addHandler(handler)
+    # set to DEBUG to make sure all messages get logged
+    # https://docs.python.org/2/library/logging.html#logging.Logger.setLevel
+    logger.setLevel(logging.DEBUG)
+    return logger
