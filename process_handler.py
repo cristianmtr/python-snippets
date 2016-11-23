06675748649bbef31aec79804932fb1e97011937
Cristian Mtr <cristianmtr@users.noreply.github.com>
2016-11-23 15:29:42 +0100

    Update process_handler.py

--- process_handler.py
+++ process_handler.py
@@ -75,7 +75,10 @@ def shell_ex(cmd, print_output=True, outputfile=None, outerrfile=None):
     return exitcode, stdout, stderr            
 
 
-def popen_improved(command, shell_cmd=False, print_output=False):
+def popen_improved(command, shell_cmd=False,
+                   print_output=True,
+                   buffer_output=False,
+                   cwdir=os.curdir):
     """
     Parameters
     ----------
@@ -88,9 +91,12 @@ def popen_improved(command, shell_cmd=False, print_output=False):
         Windows only.
         e.g. for "set", or "dir", or "where"
     print_output : bool, optional
-        print output, err, exit_code to stdout of current process
+        print all the output
+    buffer_output : bool, optional
+        buffer or send directly to stdout / stderr
     Returns object with output, err, exit_code properties
     """
+
     class PopenReturn(object):
         """
         Attributes
@@ -102,23 +108,50 @@ def popen_improved(command, shell_cmd=False, print_output=False):
         output : string
             stdout from process, stripped
         """
+
         def __init__(self, output, err, exit_code):
             super(PopenReturn, self).__init__()
             self.output = output
             self.err = err
             self.exit_code = exit_code
-    if print_output:
-        logger.info("EXECUTING '{}'".format(" ".join(command)))
-    process = Popen(command, stdout=PIPE, stderr=PIPE, shell=shell_cmd)
-    output = ""
-    err = ""
-    for line in process.stdout.readlines():
-        output += line
-    for error_line in process.stderr.readlines():
-        err += error_line
-    exit_code = process.wait()
-    if print_output:
-        logger.info("OUTPUT: {}".format(output))
-        logger.info("ERR: {}".format(err))
-        logger.info("EXIT CODE: {}".format(exit_code))
-    return PopenReturn(output.strip(), err.strip(), exit_code)
+
+    logger.info("EXECUTING '%s'", " ".join(command))
+
+    if buffer_output:
+        process = Popen(command,
+                        stdout=PIPE,
+                        stderr=PIPE,
+                        shell=shell_cmd,
+                        cwd=cwdir,
+                        bufsize=1)
+        output = ""
+        err = ""
+
+        output, err = process.communicate()
+        exit_code = process.returncode
+
+        # special treatment for MConfig
+        if "MConfig" in command[0]:
+            mconfig_log_file = command[1]+".log"
+            logger.info("popen_improved reading MConfig output from %s",
+                        mconfig_log_file)
+            with open(mconfig_log_file) as mconfig_log_contents:
+                for line in mconfig_log_contents.readlines():
+                    output += line
+
+        if print_output:
+            logger.info("STDOUT: %s", output)
+            logger.info("STDERR: %s", err)
+            logger.info("EXIT CODE: %s", exit_code)
+
+        return PopenReturn(output.strip(), err.strip(), exit_code)
+
+    else:
+        process = Popen(command,
+                        shell=shell_cmd,
+                        cwd=cwdir,
+                        bufsize=1)
+
+        exit_code = process.wait()
+
+        return PopenReturn("", "", exit_code)
