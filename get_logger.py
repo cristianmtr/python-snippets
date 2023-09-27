07441a117198d98a10143eb6aa4fe1e4ffdb17e7
cristian <cristianmtr@users.noreply.github.com>
2023-09-27 16:50:30 +0200

    Update get_logger.py

--- get_logger.py
+++ get_logger.py
@@ -1,38 +1,23 @@
 import logging
+from logging import StreamHandler
 
 
-# Python's logging framework creates a single instance
-# for each 'name' logger
-# we need to make sure to return the same
-# instance for the same 'name'
-# e.g. Runnable class instances use the same logger
-# see http://stackoverflow.com/questions/7173033/duplicate-log-output-when-using-python-logging-module
-loggers = {}
+def get_logger(name):
+    log_format = "%(asctime)s %(name)s : %(levelname)s : %(message)s"
 
-def get_module_logger(name):
-    """Get a logger for the module name
+    loggers = [logging.getLogger()]  # get the root logger
+    loggers = loggers + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    for l in loggers:
+        l.handlers.clear()
 
-    Args:
-        name (string): name of the module
+    logging.basicConfig(
+        level='INFO',
+        format=log_format,
+        datefmt='[%X]',
+        handlers=[StreamHandler()],
+        force=True
+    )
 
-    Returns:
-        TYPE: (logger)[https://docs.python.org/2/library/logging.html#logging.Logger]\
-             instance.
-
-    """
-    global loggers
-    if loggers.get(name):
-        return loggers.get(name)
-    else:
-        return_logger = logging.getLogger(name)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            datefmt='%Y-%m-%d %H:%M')
-        handler.setFormatter(formatter)
-        return_logger.addHandler(handler)
-        # set to DEBUG to make sure all messages get logged
-        # https://docs.python.org/2/library/logging.html#logging.Logger.setLevel
-        return_logger.setLevel(logging.DEBUG)
-        loggers[name] = return_logger
-        return return_logger
+    logger = logging.getLogger(name, )
+    logger.info(f"initialized logger for {name}")
+    return logger
